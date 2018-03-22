799bc2c95be0fa72711c5b26f1b7e3ae4e57726d
Charlie Yin <charlieyinjunhao@hotmail.com>
2018-03-22 02:11:04 -0700

    Changed optimization level to O3. Added definition to disable eigen assertions.

--- main.cc
+++ main.cc
@@ -1,14 +1,15 @@
+#define EIGEN_NO_DEBUG
 #include <iostream>
 #include "lstm.h"
 
 int main(int argc, char const* argv[])
 {
 	std::srand(std::time(0));
-	LSTM network(128, 128, 128, 0.001);
+	LSTM network(128, 128, 128, 0.002);
 	network.load("./samples/shakespear.txt");
 	network.loadState("./data/weights.txt");
-	// network.saveTo("./data/weights.txt");
-	// network.train(5, 100);
-	network.output(1000);
+	network.saveTo("./data/weights.txt");
+	network.train(50, 100);
+	network.output(300);
 	return 0;
 }
